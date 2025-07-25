10498e50c260da7f8a29426fe39fe7240a50ec50
Olalekan Bamigboye <olalekanbamigboye732@gmail.com>
2025-07-25 17:52:40 +0100

    mongo_db setup and connection

--- database/database.js
+++ database/database.js
@@ -3,8 +3,8 @@ require("dotenv").config()
 
 const dataBase = async (req, res)=>{
     try {
-       
       await mongoose.connect(process.env.MONGO_URL)  
+      console.log("database connected")
     } catch (e) {
        console.error("something went wrong", e.message)
        process.exit(1)   
